9b068520fdd59466692a1f0586c7541fce426861
newplayer <2035282956@qq.com>
2023-03-20 10:51:40 +0800

    增加便利ast数功能

--- src/traverser.js
+++ src/traverser.js
@@ -15,13 +15,17 @@ function traverser(ast, visitor) {
       methods.enter(node, parent)
     }
     switch (node.type) {
-      case 'program':
-        traverserArray(node.body, node)
+      case 'BlockStatementLeft':
+        traverserArray(node.BlockStatementLeft, node)
         break
-      case 'CallExpression':
-        traverserArray(node.params, node)
+      case 'ParentStatementLeft':
+        traverserArray(node.ParentStatementLeft, node)
         break
-      case 'NumberLiteral':
+      case 'ArrayExpressionLeft':
+        traverserArray(node.ArrayExpressionLeft, node)
+        break
+      case 'CustomNameStatement':
+      case 'NumberStatement':
         break
       default:
         break
@@ -29,9 +33,114 @@ function traverser(ast, visitor) {
     if (methods && methods.exit) {
       methods.exit(node, parent)
     }
-  } 
+  }
   traverserNode(ast, null)
 }
 
 
 export default traverser
+let visitor = {
+  program:{
+    enter(node,parent){
+      parent._context.push({
+        type:'program',
+        value:node.type
+      })
+    },
+
+  },
+  NumberStatement: {
+    enter(node, parent) {
+      parent._context.push({
+        type: 'NumberStatement',
+        value: node.value
+      });
+    },
+  },
+  CustomNameStatement: {
+    enter(node, parent) {
+      parent._context.push({
+        type: 'CustomNameStatement',
+        value: node.value
+      });
+    },
+  },
+  ParentStatement: {
+    enter(node, parent) {
+      let expression = {
+        type: 'ParentStatement',
+        //targetfunction:目标函数
+        targetfunction: {
+          type: 'nameoffunction',
+          name: node.ParentStatement
+        },
+        arguments: [],
+      };
+      node._context = expression.arguments;
+      if (parent.type !== 'ParentStatement') {
+        //expression:表达式
+        expression = {
+          //ExpressionStatement:表达式语句
+          type: 'ExpressionStatement',
+          expression: expression,
+        };
+      }
+      parent._context.push(expression);
+    },
+  }
+}
+function transformer(ast,visitor) {
+  let newAst = {
+    type: 'Program',
+    body: [],
+  };
+  ast._context = newAst.body;
+  traverser(ast, visitor);
+  return newAst;
+}
+
+const ast = {
+  "type": "Program",
+    "body": [
+      {
+        "type": "FunctionDeclaration",
+        "value": "function"
+      },
+      {
+        "type": "CustomNameStatement",
+        "value": "add"
+      },
+      {
+        "type": "ParentStatement",
+        "value": "x",
+        "body": [
+          {
+            "type": "CommaStatement",
+            "value": ","
+          },
+          {
+            "type": "CustomNameStatement",
+            "value": "y"
+          }
+        ]
+      },
+      {
+        "type": "BlockStatement",
+        "value": "return",
+        "body": [
+          {
+            "type": "CustomNameStatement",
+            "value": "x"
+          },
+          {
+            "type": "AdditionStatement",
+            "value": "+"
+          },
+          {
+            "type": "CustomNameStatement",
+            "value": "y"
+          }
+        ]
+      }
+    ]
+}
