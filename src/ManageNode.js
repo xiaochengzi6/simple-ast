49964e63c54213ea0fe5d132ac1fedee294c3648
小橙子 <a2742217795@hotmail.com>
2023-03-21 10:02:18 +0800

    fix: 尝试对 parser 进行处理

---
+++ src/ManageNode.js
@@ -0,0 +1,12 @@
+
+// manageTokens 
+/**
+ * 管理 tokens 
+ */
+class ManageNode {
+  constructor() {
+    this.state = 0
+    this.end = 0
+  }
+  
+}
--- src/parser.js
+++ src/parser.js
@@ -1,4 +1,4 @@
-import { checkRight, tokenTypeName } from "../utils/index.js"
+import { checkRight, KeywordType } from "../utils/index.js"
 
 /**
  * 生成 ast
@@ -10,84 +10,66 @@ function parser(tokens) {
   function walk() {
     let token = tokens[current]
     checkRight(token, `解析 token 出现问题 current: ${current}`)
-    
-    const { type, value } = token
-    const targetObj = tokenTypeName[type]
-    checkRight(targetObj, `取不到 tokenTypeName 中的 type 当前type:${type}`)
 
-    const { type: targetObjType, recursion } = targetObj
-
-    console.log(recursion)
-    // 递归处理 括号 花括号
-    if (recursion == true) {
-      // 花括号
-      if (value === '{') {
-        token = tokens[++current]
-
-        const node = {
-          type: "BlockStatement",
-          value: token.value,
-          body: []
-        }
-
-        token = tokens[++current]
-
-        while (token.value !== '}') {
-          node.body.push(walk())
-          token = tokens[current]
-        }
-
-        // 跳过 "}"
-        current ++
-        return node
-      }
-
-      // 括号
-      if (value === '(') {
-        token = tokens[++current]
-
-        const node = {
-          type: "ParentStatement",
-          value: token.value,
-          body: []
-        }
-
-        token = tokens[++current]
-
-        while (token.value !== ")") {
-          node.body.push(walk())
-          token = tokens[current]
-        }
-
-        // 跳过 ')'
-        current ++
-        return node
-      }
-
-      // 数组
-      if (value === '[') {
-        token = tokens[++current]
-
-        const node = {
-          type: "ArrayExpression",
-          element: []
-        }
-
-        while (token.type !== ']') {
-          node.element.push(walk())
-          current++
-        }
-
-        return node
-      }
-    }
-    else {
-      // 默认的
-      current ++
-      return {
-        type: targetObjType,
-        value
-      }
+    // 在这里去进行关键词判断
+    switch (type) {
+      case 'switch':
+      case 'break':
+        break
+      case 'case':
+        break
+      case 'catch':
+        break
+      case 'class':
+        break
+      case 'continue':
+        break
+      case 'debugger':
+        break
+      case 'default':
+        break
+      case 'delete':
+        break
+      case 'do':
+        break
+      case 'else':
+        break
+      case 'false':
+        break
+      case 'in':
+        break
+      case 'if':
+        break
+      case 'true':
+        break
+      case 'break':
+        break
+      case 'var':
+        break
+      case 'let':
+        break
+      case 'const':
+        break
+      case 'function ':
+        break
+      case 'with':
+        break
+      case 'for':
+        break
+      case 'try':
+        break
+      case 'catch':
+        break
+      case 'null':
+        break
+      case 'new':
+        break
+      case 'instanceof':
+        break
+      case 'typeof':
+        break
+      default:
+        break
     }
   }
 
